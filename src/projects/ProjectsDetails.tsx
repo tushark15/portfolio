d6020c99fca88e3a38cc4261b3b87654bb7d7b45
Tushar <tusharkhandelwal1512@gmail.com>
2023-11-29 14:32:52 +0530

    Minor change

--- src/projects/ProjectsDetails.tsx
+++ src/projects/ProjectsDetails.tsx
@@ -16,17 +16,6 @@ interface projectType {
 
 export const projects: projectType[] = [
   {
-    title: "Your Places",
-    img: yourPlaces,  
-    skills: ["React - ", "Node - ", "MongoDB - ", "Express - ", "JWT"],
-    desc: "Embark on a virtual journey sharing your travel tales! This platform lets you showcase the places you've explored, connecting users worldwide through wanderlust-inspiring photos and experiences.",
-    largeDesc:
-      "'Your Places' is a unique social media platform crafted for avid travelers and adventure enthusiasts, allowing users to share their globe-trotting escapades with the world. From hidden gems to iconic landmarks, users can post vivid descriptions, photos, and tips about the places they've visited, creating an interactive map of personal adventures. Whether it's a bustling city or a serene mountain retreat, this platform serves as a digital travel journal, inspiring and connecting users while offering a window into diverse destinations worldwide. Embrace the wanderlust and explore the globe through the experiences and recommendations of fellow explorers on 'Your Places.'",
-    frontend: "https://github.com/tushark15/your_places_frontend",
-    backend: "https://github.com/tushark15/your_places_backend",
-    live:`${import.meta.env.VITE_APP_YP_LINK}`
-  },
-  {
     title: "Online Homework Management System",
     img: ohms,
     skills: ["MERN - ", "AWS - ", "JWT - ", "React-Bootstrap"],
@@ -35,7 +24,7 @@ export const projects: projectType[] = [
       '"Introducing the Online Homework Management System, your go-to platform for seamless and organized academic success. This innovative system empowers students to manage their assignments, deadlines, and academic tasks efficiently. With user-friendly interfaces and intuitive features, it simplifies the process of submitting homework, and tracking deadlines. The platform fosters a conducive environment for learning, monitor progress, and ensure timely completion of assignments. Elevate your academic journey with this comprehensive and user-centric Online Homework Management System, designed to enhance productivity and promote academic excellence."',
     frontend: "https://github.com/tushark15/OHMS_frontend",
     backend: "https://github.com/tushark15/OHMS_backend",
-    live:`${import.meta.env.VITE_APP_OHMS_LINK}`
+    live: `${import.meta.env.VITE_APP_OHMS_LINK}`,
   },
   {
     title: "Flickr Photo Search App",
@@ -54,7 +43,7 @@ export const projects: projectType[] = [
     desc: "Streamline your task management effortlessly with our robust CRUD operations. This versatile API simplifies creating, reading, updating, and deleting tasks, offering a seamless integration for efficient to-do list management across applications",
     largeDesc: `"Introducing TodoApp API, your solution for streamlined task management. This robust API offers seamless Create, Read, Update, and Delete (CRUD) operations, providing developers with powerful tools to manage tasks effectively. With its intuitive endpoints, developers can easily integrate the API into their applications, enabling users to create, view, update, and delete tasks effortlessly. Designed with simplicity and functionality in mind, TodoApp API ensures a smooth and reliable experience for managing to-do lists across various platforms. Enhance your application's task management capabilities with the TodoApp API, empowering users to stay organized and productive."`,
     backend: "https://github.com/tushark15/todoapp-api",
-    live: `${import.meta.env.VITE_APP_TODO_LINK}`
+    live: `${import.meta.env.VITE_APP_TODO_LINK}`,
   },
 
   {
@@ -65,7 +54,17 @@ export const projects: projectType[] = [
     largeDesc:
       "This project employs React, a library for constructing the user interface, offering flexibility and ease in UI development. It leverages Vite to expedite the development and building process, ensuring efficiency. The codebase is exclusively written in TypeScript, enhancing type safety and facilitating a more robust developer experience. To style its components, the project utilizes styled-components, which provide a convenient and flexible way to apply styles. It demonstrates a straightforward login page UI equipped with fundamental functionalities, using styled-components to elegantly style and enhance the appearance and behavior of the login form components. Overall, the project combines React, Vite, TypeScript, and styled-components to create a structured, efficient, and visually appealing login page with a focus on user experience and development convenience.  ",
     frontend: "https://github.com/tushark15/ReactLogin",
-    live:`${import.meta.env.VITE_APP_RLP_LINK}`
+    live: `${import.meta.env.VITE_APP_RLP_LINK}`,
   },
-  
+  {
+    title: "Your Places",
+    img: yourPlaces,
+    skills: ["React - ", "Node - ", "MongoDB - ", "Express - ", "JWT"],
+    desc: "Embark on a virtual journey sharing your travel tales! This platform lets you showcase the places you've explored, connecting users worldwide through wanderlust-inspiring photos and experiences.",
+    largeDesc:
+      "'Your Places' is a unique social media platform crafted for avid travelers and adventure enthusiasts, allowing users to share their globe-trotting escapades with the world. From hidden gems to iconic landmarks, users can post vivid descriptions, photos, and tips about the places they've visited, creating an interactive map of personal adventures. Whether it's a bustling city or a serene mountain retreat, this platform serves as a digital travel journal, inspiring and connecting users while offering a window into diverse destinations worldwide. Embrace the wanderlust and explore the globe through the experiences and recommendations of fellow explorers on 'Your Places.'",
+    frontend: "https://github.com/tushark15/your_places_frontend",
+    backend: "https://github.com/tushark15/your_places_backend",
+    live: `${import.meta.env.VITE_APP_YP_LINK}`,
+  },
 ];
